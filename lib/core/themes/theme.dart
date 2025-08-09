689de20fbdc9836b395ee7ad2795a7710a6cd68c
Aryan <pakhrinp242@gmail.com>
2025-08-09 22:29:42 +0545

    main

--- lib/core/themes/theme.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-class MyTheme {
-  static Color reddishColor = Color(0xFFFF8F8F); // Light red
-}
--- lib/routes/app_routes.dart
+++
@@ -1,8 +0,0 @@
-
-
-class MyRoutes {
-  static String splashScreenRoute = "/SplashScreen";
-  static String loginScreenRoute = "/LoginScreen";
-  static String registerScreenRoute = "/RegisterScreen";
-  static String forgotPasswordSreenRoute = "/ForgotPasswordScreen";
-}
